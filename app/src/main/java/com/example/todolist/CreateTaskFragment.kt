6855f1fab4cb0de046d315f5b6aeb9a9d260f0bc
Alex Brito <ajbrito93@gmail.com>
2024-04-10 20:12:29 -0700

    Navigation Component + Initial Navigation Behavior

--- app/src/main/java/com/example/todolist/CreateTaskFragment.kt
+++ app/src/main/java/com/example/todolist/CreateTaskFragment.kt
@@ -1,7 +1,14 @@
 package com.example.todolist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class CreateTaskFragment: Fragment(R.layout.create_new_task) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.todolist
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.todolist.entity.PersonalTask
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val TAG : String = "MyActivity"
@@ -13,10 +15,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null){
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<TaskListFragment>(R.id.main_fragment_container_view)
-            }
+            val taskListFragment = TaskListFragment()
+            val floatingActionButton = findViewById<FloatingActionButton>(R.id.floating_action_button)
+            supportFragmentManager.beginTransaction().add(R.id.main_fragment_container_view, taskListFragment, TaskListFragment.TAG).commit()
         }
 
     }
